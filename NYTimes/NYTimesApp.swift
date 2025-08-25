6d9f9a9a8e71a4fa2349a2afd0dafe546c6e4b75
Omar Abdulrahman <omeralhadu@icloud.com>
2025-08-25 20:20:10 +0400

    Initial Commit

---
+++ NYTimes/NYTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesApp.swift
+//  NYTimes
+//
+//  Created by Omar Abdulrahman on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
